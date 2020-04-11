57c967a57c2458fd4d6bb21f537a758d03aa797f
junepark678 <40409848+junepark678@users.noreply.github.com>
2020-04-11 10:04:53 +0900

    Initial Commit

---
+++ todo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todo2
+//
+//  Created by June P on 2020/04/11.
+//  Copyright © 2020 June P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
